e829e8802553b558e9d2b6f1add4cf2a15031431
CHUNG CHIH-HUNG <56349177+CCH-0319@users.noreply.github.com>
2025-10-09 10:57:03 +0800

    Update omv_boardconfig.h

--- boards/STM32H743VGT6/omv_boardconfig.h
+++ boards/STM32H743VGT6/omv_boardconfig.h
@@ -76,7 +76,7 @@
 
 //PLL1 480MHz/48MHz for USB, SDMMC and FDCAN
 #define OMV_OSC_PLL1M                         (5)       // 25/5=5 MHz
-#define OMV_OSC_PLL1N                         (192/2)     // 5*192=960 MHz (VCO)
+#define OMV_OSC_PLL1N                         (192 / 2)     // 5*192=960 MHz (VCO)
 #define OMV_OSC_PLL1P                         (2)       // 960/2=480 MHz (SYSCLK)
 #define OMV_OSC_PLL1Q                         (20)      // 960/20=48 MHz (USB)
 #define OMV_OSC_PLL1R                         (2)       // 960/2=480 MHz
